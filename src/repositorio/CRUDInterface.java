081e3c1c59d07b9e0293d69b166331c43db3d05e
Yasmin Marinho <yasmimarinhofalcao@gmail.com>
2026-06-23 18:07:21 -0300

    feat: adiciona classe GeradorID e interface

--- src/repositorio/CRUDInterface.java
+++ src/repositorio/CRUDInterface.java
@@ -1,1 +1,14 @@
 package repositorio;
+import java.util.List;
+
+//padronizaçao dos metodos dos repositorios, para facilitar a manutencao e a evolucao do sistema
+public interface CRUDInterface<T> {
+	public void criar(T obj);
+	public T localizar(int id);
+	public T atualizar(T obj);
+	public void deletar(T obj) ;
+	public List<T> listar();
+	public void begin();
+	public void commit();
+	public void rollback();
+}
